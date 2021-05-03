2de93d1f6c3ff72a7841825d692b7200005b680e
Rowan Evenstar <zenrhe@gmail.com>
2021-05-03 17:45:29 +0100

    erm..

---
+++ clip-table/includes/class-clip-table-deactivator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/rowanevenstar/
+ * @since      1.0.0
+ *
+ * @package    clip-table
+ * @subpackage clip-table/includes
+ */
+
+/**
+ * Fired during plugin de-activation.
+ *
+ * This class defines all code necessary to run during the plugin's de-activation.
+ *
+ * @since      1.0.0
+ * @package    clip-table
+ * @subpackage clip-table/includes
+ * @author     Rowan Evenstar <rowan.evenstar@me.com>
+ */
+class Clip_Table_Deactivator {
+
+	/**
+	 * Currently Nothing Happes on Deactivate
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+		//Likely Delete table
+	}
+
+}
